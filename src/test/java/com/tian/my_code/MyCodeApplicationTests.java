ebd8a115fc275ddd4a00dd29e69fa3649a3f7bca
laotian <251965157@qq.com>
2020-11-08 10:21:14 +0800

    初始化提交项目代码

---
+++ src/test/java/com/tian/my_code/MyCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tian.my_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
